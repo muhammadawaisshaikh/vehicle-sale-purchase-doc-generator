4a0dfdca2967add644aa85d36ff2d53546096a75
Muhammad Awais <awais.shaikh@noice.id>
2023-03-26 18:27:51 +0500

    improvements v1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const USER = {
-    cnic: '45504-1105237-3',
-    name: 'MUHAMMAD IQBAL SHAIKH',
-    fathersName: 'ABDUL LATIF SHAIKH',
-    contact: '+923003143891',
-    address: 'House No. A-55, Shalimar Bungalows Gate III, Near Mobile Mall, Block 17, Gulistan-e-Johar, Karachi',
-    residentialAddress: 'House No. A-55, Shalimar Bungalows Gate III, Near Mobile Mall, Block 17, Gulistan-e-Johar, Karachi'
+    cnic: 'Your CNIC',
+    name: 'Your Name',
+    fathersName: 'Your Fathers Name',
+    contact: 'Your Contact',
+    address: 'Your Address',
+    residentialAddress: 'Your Address Residential'
 }
