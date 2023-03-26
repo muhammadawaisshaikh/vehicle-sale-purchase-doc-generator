8b3ceb1e2b2399b0536d04d5f81372fb932702af
Muhammad Awais <awais.shaikh@noice.id>
2023-03-26 19:39:02 +0500

    main page UI improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,9 @@ export default function Home() {
     <>
       <main className={styles.main}>
         <div className={styles.description}>
-          <h1>Vehicle Sale Purchase Doc Generator</h1>
-          <p>sale receipt, transfer letter and delivery letter generator application built with ❤️ using nextjs</p>
+          <img className={styles.imageMain} src='https://i.ibb.co/fVRspK9/car-sell.png' />
+          <h1 className='pt-5'>Vehicle Sale Purchase Doc Generator</h1>
+          <p className='mb-3'>sale receipt, transfer letter and delivery letter generator application built with ❤️ using nextjs</p>
           <Link className="btn btn-primary" href="/first-party">Get Started</Link>
         </div>
       </main>
